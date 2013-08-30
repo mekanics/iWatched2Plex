0e6292391d1120fa4d77035409359d3128796362
Alexandre Joly <alexandre.joly@mekanics.ch>
2013-08-30 08:49:13 +0200

    default values for the options

--- lib/iwatched2plex/cli.rb
+++ lib/iwatched2plex/cli.rb
@@ -8,13 +8,13 @@ module IWatched2Plex
 		default_task :sync
 
 		desc "sync", "sync the Plex library with the iTunes library"
-		method_option :itunes, :type => :string, :aliases => ["-i"], :desc => "The path to your iTunes library [~/Music/iTunes]"
-		method_option :host, :type => :string, :aliases => ["-h"], :desc => "Your Plex host [localhost]"
-		method_option :port, :type => :numeric, :aliases => ["-p"], :desc => "The Port of your Plex Server [32400]"
+		method_option :itunes, :type => :string, :aliases => ["-i"], :default => "~/Music/iTunes", :desc => "The path to your iTunes library [~/Music/iTunes]"
+		method_option :host, :type => :string, :aliases => ["-h"], :default => "localhost", :desc => "Your Plex host [localhost]"
+		method_option :port, :type => :numeric, :aliases => ["-p"], :default => 32400, :desc => "The Port of your Plex Server [32400]"
 		def sync
-			itunes_lib_path = options.itunes.nil? ? File.expand_path("~/Music/iTunes") : File.expand_path(options.itunes)
-			plex_host = options.host.nil? ? "localhost" : options.host
-			plex_port = options.port.nil? ? 32400 : options.port
+			itunes_lib_path = File.expand_path(options.itunes)
+			plex_host = options.host
+			plex_port = options.port
 
 			puts "#{itunes_lib_path} --> #{plex_host}:#{plex_port}"
 
